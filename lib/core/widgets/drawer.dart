9d86186729fdcaa96413144e8ff16757f4381814
Antara Paul <hiiamantara04@gmail.com>
2025-02-07 18:01:49 +0530

    Fix: Icons and truncated text

--- lib/core/widgets/drawer.dart
+++ lib/core/widgets/drawer.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class FloatingDrawer extends StatefulWidget {
-
   const FloatingDrawer({super.key});
 
   @override
@@ -10,46 +9,41 @@ class FloatingDrawer extends StatefulWidget {
 }
 
 class _FloatingDrawerState extends State<FloatingDrawer> {
-  bool isOpen = false; // Controls drawer state
-  bool isEditing = false; // To toggle between text and textfield
+  bool isOpen = false;
+  bool isEditing = false;
   final TextEditingController _controller = TextEditingController();
-  bool isHovered = false; // To detect hover effect
 
   @override
   void initState() {
     super.initState();
-    _controller.text = 'Cooketh Flow'; // Initial text
+    _controller.text = 'Cooketh Flow';
   }
 
   void toggleDrawer() {
     setState(() {
-      isOpen = !isOpen; // Toggle drawer state
+      isOpen = !isOpen;
     });
+  }
+
+  String getTruncatedTitle() {
+    String text = _controller.text;
+    return text.length > 12 ? text.substring(0, 12) + '...' : text;
   }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        // Background overlay to close the drawer when tapped
-        GestureDetector(
-          child: SizedBox(
-            width: double.infinity,
-            height: double.infinity,
-          )
-        ),
-        // Background overlay (only when open)
         if (isOpen)
           GestureDetector(
-            onTap: toggleDrawer, // Close drawer when tapping outside
+            onTap: toggleDrawer,
             child: Container(
               width: double.infinity,
               height: double.infinity,
-              color: Colors.black.withOpacity(0),
+              color: Colors.black.withOpacity(0.5),
             ),
           ),
-        
-        // Drawer Button (always visible)
+
         Positioned(
           top: 24,
           left: 24,
@@ -59,15 +53,15 @@ class _FloatingDrawerState extends State<FloatingDrawer> {
               width: 250,
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
               decoration: BoxDecoration(
-                color: isOpen ? Colors.blue : Colors.white, // Background changes when open
+                color: isOpen ? Colors.blue : Colors.white,
                 borderRadius: BorderRadius.circular(12),
                 border: Border.all(color: Colors.black, width: 1),
               ),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween, // Ensures even spacing
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    "Cooketh Flow",
+                    getTruncatedTitle(),
                     style: TextStyle(
                       fontSize: 20,
                       color: isOpen ? Colors.white : Colors.black,
@@ -75,9 +69,13 @@ class _FloatingDrawerState extends State<FloatingDrawer> {
                       fontFamily: 'Frederik',
                     ),
                   ),
-                  Icon(
-                    PhosphorIconsRegular.sidebarSimple,
-                    color: isOpen ? Colors.white : Colors.black,
+                  GestureDetector(
+                    onTap: toggleDrawer,
+                    child: Icon(
+                      isOpen ? PhosphorIconsFill.xCircle : PhosphorIconsFill.sidebarSimple,
+                      color: isOpen ? Colors.white : Colors.black,
+                      size: 24,
+                    ),
                   ),
                 ],
               ),
@@ -85,8 +83,6 @@ class _FloatingDrawerState extends State<FloatingDrawer> {
           ),
         ),
 
-
-        // Floating drawer (only when open)
         if (isOpen)
           Positioned(
             top: 24,
@@ -104,54 +100,56 @@ class _FloatingDrawerState extends State<FloatingDrawer> {
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    GestureDetector(
-                      onDoubleTap: () {
-                        setState(() {
-                          isEditing = !isEditing; // Toggle editing state
-                        });
-                      },
-                      child: MouseRegion(
-                        onEnter: (_) => setState(() => isHovered = true),
-                        onExit: (_) => setState(() => isHovered = false),
-                        child: Container(
-                          padding: EdgeInsets.all(16),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
-                          ),
-                          child: Align(
-                            alignment: Alignment.centerLeft,
+                    Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          GestureDetector(
+                            onDoubleTap: () {
+                              setState(() {
+                                isEditing = true;
+                              });
+                            },
                             child: isEditing
-                                ? TextField(
-                                    controller: _controller,
-                                    decoration: InputDecoration(border: OutlineInputBorder()),
-                                    style: TextStyle(
-                                      fontFamily: 'Frederik',
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                    onSubmitted: (_) => setState(() => isEditing = false),
-                                  )
-                                : AnimatedContainer(
-                                    duration: Duration(milliseconds: 200),
-                                    padding: EdgeInsets.all(8),
-                                    decoration: BoxDecoration(
-                                      border: isHovered
-                                          ? Border(bottom: BorderSide(color: Colors.blue, width: 2))
-                                          : Border.all(color: Colors.transparent),
-                                    ),
-                                    child: Text(
-                                      _controller.text,
+                                ? SizedBox(
+                                    width: 150,
+                                    child: TextField(
+                                      controller: _controller,
+                                      decoration: InputDecoration(border: OutlineInputBorder()),
                                       style: TextStyle(
                                         fontFamily: 'Frederik',
                                         fontSize: 20,
                                         fontWeight: FontWeight.bold,
                                       ),
+                                      onSubmitted: (_) {
+                                        setState(() {
+                                          isEditing = false;
+                                        });
+                                      },
+                                    ),
+                                  )
+                                : Text(
+                                    getTruncatedTitle(),
+                                    style: TextStyle(
+                                      fontFamily: 'Frederik',
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
                                     ),
                                   ),
                           ),
-                        ),
+                          GestureDetector(
+                            onTap: toggleDrawer,
+                            child: Icon(
+                              PhosphorIconsRegular.sidebarSimple,
+                              color: Colors.black,
+                              size: 24,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
+                    Divider(),
                     ListTile(title: Text('Option 1')),
                     ListTile(title: Text('Option 2')),
                   ],

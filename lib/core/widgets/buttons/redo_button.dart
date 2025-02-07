0817c538887fb939aa7702f325ed4a50f04af0a2
Antara Paul <hiiamantara04@gmail.com>
2025-02-08 03:25:54 +0530

    Add: Initial Toolbar UI

---
+++ lib/core/widgets/buttons/redo_button.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+
+class RedoButton extends StatelessWidget {
+  final VoidCallback? onRedo;
+
+  const RedoButton({super.key, this.onRedo});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(PhosphorIconsRegular.arrowUUpRight),
+      onPressed: onRedo,
+      tooltip: "Redo",
+    );
+  }
+}

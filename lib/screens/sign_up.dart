f70168ff13d3255d2aa0bd0ac2317423b9e10b8e
Antara Paul <hiiamantara04@gmail.com>
2025-02-12 00:21:39 +0530

    Add: Signup page ui + small fixes in login page ui

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
+class SignupPage extends StatefulWidget {
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _SignupPageState createState() => _SignupPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _SignupPageState extends State<SignupPage> {
   bool _obscurePassword = true;
+  bool _obscureConfirmPassword = true;
 
   @override
   Widget build(BuildContext context) {
@@ -22,28 +23,40 @@ class _LoginPageState extends State<LoginPage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    "Log In",
+                    "Sign Up",
                     style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, fontFamily: 'Frederik'),
                   ),
                   SizedBox(height: 8),
                   Text(
-                    "Log in to existing account",
+                    "Create a new account",
                     style: TextStyle(fontSize: 20, color: Colors.grey, fontFamily: 'Frederik', fontWeight: FontWeight.w200),
                   ),
                   SizedBox(height: 24),
-                  Text("Username / Email Address", style: TextStyle(fontSize: 20, fontFamily: 'Frederik')),
+                  
+                  Text("Username", style: TextStyle(fontSize: 20, fontFamily: 'Frederik')),
                   SizedBox(height: 8),
                   TextField(
                     decoration: InputDecoration(
                       contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
                       border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                      hintText: 'Enter your Username / Email Address',
+                      hintText: 'Enter your Username',
                       hintStyle: TextStyle(color: Colors.grey, fontFamily: 'Frederik', fontSize: 16),
-                      disabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black, width: 1)),
-                      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.blue, width: 2), borderRadius: BorderRadius.circular(12)),
                     ),
                   ),
                   SizedBox(height: 24),
+                  
+                  Text("Email Address", style: TextStyle(fontSize: 20, fontFamily: 'Frederik')),
+                  SizedBox(height: 8),
+                  TextField(
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+                      hintText: 'Enter your Email Address',
+                      hintStyle: TextStyle(color: Colors.grey, fontFamily: 'Frederik', fontSize: 16),
+                    ),
+                  ),
+                  SizedBox(height: 24),
+
                   Text("Password", style: TextStyle(fontSize: 20, fontFamily: 'Frederik')),
                   SizedBox(height: 8),
                   TextField(
@@ -51,10 +64,8 @@ class _LoginPageState extends State<LoginPage> {
                     decoration: InputDecoration(
                       contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
                       border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                      hintText: 'Enter the Password',
+                      hintText: 'Enter your Password',
                       hintStyle: TextStyle(color: Colors.grey, fontFamily: 'Frederik', fontSize: 16, fontWeight: FontWeight.w100),
-                      disabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black, width: 1)),
-                      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.blue, width: 2), borderRadius: BorderRadius.circular(12)),
                       suffixIcon: IconButton(
                         icon: Icon(_obscurePassword ? Icons.visibility_off : Icons.visibility),
                         onPressed: () {
@@ -66,6 +77,28 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ),
                   SizedBox(height: 24),
+
+                  Text("Confirm Password", style: TextStyle(fontSize: 20, fontFamily: 'Frederik')),
+                  SizedBox(height: 8),
+                  TextField(
+                    obscureText: _obscureConfirmPassword,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+                      hintText: 'Enter your Password again',
+                      hintStyle: TextStyle(color: Colors.grey, fontFamily: 'Frederik', fontSize: 16, fontWeight: FontWeight.w100),
+                      suffixIcon: IconButton(
+                        icon: Icon(_obscureConfirmPassword ? Icons.visibility_off : Icons.visibility),
+                        onPressed: () {
+                          setState(() {
+                            _obscureConfirmPassword = !_obscureConfirmPassword;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 24),
+
                   Center(
                     child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
@@ -77,21 +110,22 @@ class _LoginPageState extends State<LoginPage> {
                       onPressed: () {},
                       child: Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
-                        child: Text("Log in", style: TextStyle(fontSize: 16, fontFamily: 'Frederik')),
+                        child: Text("Sign up", style: TextStyle(fontSize: 16, fontFamily: 'Frederik')),
                       ),
                     ),
                   ),
                   SizedBox(height: 16),
+
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("Don't have an account?", style: TextStyle(fontFamily: 'Frederik')),
+                      Text("Already have an account?", style: TextStyle(fontFamily: 'Frederik')),
                       TextButton(
                         onPressed: () {},
                         style: TextButton.styleFrom(
                           padding: EdgeInsets.symmetric(horizontal: 5),
                         ),
-                        child: Text("Sign up", style: TextStyle(color: Colors.blue, fontFamily: 'Frederik')),
+                        child: Text("Log in", style: TextStyle(color: Colors.blue, fontFamily: 'Frederik')),
                       ),
                     ],
                   )
@@ -99,6 +133,7 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
           ),
+
           Expanded(
             flex: 1,
             child: Container(

ba75496d447996ec2ac74e8ff8e7d8e111c768fc
Subroto Banerjee <subroto.2003@gmail.com>
2025-03-24 03:33:29 +0530

    fix: FINALLY FIXED THE GREY SCREEN OF DEATH, AAAAH

--- lib/core/widgets/toolbar.dart
+++ lib/core/widgets/toolbar.dart
@@ -12,33 +12,34 @@ class Toolbar extends StatelessWidget {
   final void Function() onUndo;
   final void Function() onRedo;
 
-  const Toolbar({super.key, required this.onUndo, required this.onRedo, required this.onDelete});
+  const Toolbar({
+    super.key,
+    required this.onUndo,
+    required this.onRedo,
+    required this.onDelete,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      right: 24,
-      top: 24,
-      child: Container(
-        padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
-          border: Border.all(color: Colors.black, width: 1),
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            UndoButton(onUndo: onUndo),
-            SizedBox(height: 12),
-            RedoButton(onRedo: onRedo),
-            SizedBox(height: 12),
-            AddButton(),
-            SizedBox(height: 12),
-            DeleteButton(onDelete: onDelete), // Use the DeleteButton that was already defined
-            SizedBox(height: 12),
-          ],
-        ),
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: Colors.black, width: 1),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          UndoButton(onUndo: onUndo),
+          const SizedBox(height: 12),
+          RedoButton(onRedo: onRedo),
+          const SizedBox(height: 12),
+          const AddButton(),
+          const SizedBox(height: 12),
+          DeleteButton(onDelete: onDelete),
+          const SizedBox(height: 12),
+        ],
       ),
     );
   }

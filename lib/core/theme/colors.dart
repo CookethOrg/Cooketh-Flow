13360edc3e03b2545caec58383232e412929b6a8
Antara Paul <hiiamantara04@gmail.com>
2025-06-26 22:45:16 +0530

    add: primary, secondary and tertiary colours

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -1,3 +1,31 @@
 import 'package:flutter/widgets.dart';
 
-const primaryColor = Color.fromARGB(255, 237, 99, 7);
+// Primary color
+const primaryColor = Color(0xFFED6325);
+
+// List of secondary colors
+const List<Color> secondaryColors = [
+  Color(0xFF30C66F), // [0] Green
+  Color(0xFFFF6B6B), // [1] Coral Red
+  Color(0xFFF59E0B), // [2] Amber
+  Color(0xFFA259FF), // [3] Purple
+  Color(0xFF1CC8C0), // [4] Cyan
+  Color(0xFFF53F5F), // [5] Pink Red
+  Color(0xFF3B82F6), // [6] Blue
+  Color(0xFF737492), // [7] Slate Gray
+  Color(0xFF000000), // [8] Black
+];
+
+// List of tertiary colors
+const List<Color> tertiaryColors = [
+  Color(0xFFB9F5CC), // [0] Light Green
+  Color(0xFFFFC4BC), // [1] Light Coral
+  Color(0xFFFAD7A0), // [2] Light Amber
+  Color(0xFFD4B6F5), // [3] Light Purple
+  Color(0xFFB9E8FF), // [4] Light Cyan
+  Color(0xFFF8B6BF), // [5] Light Pink
+  Color(0xFFB3D4FC), // [6] Light Blue
+  Color(0xFFC8C9E1), // [7] Light Slate
+  Color(0xFFC9C9C9), // [8] Light Gray
+  Color(0xFFFFB89E), // [9] Peach
+];

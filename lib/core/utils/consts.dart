2bf12d304bca3221246b99cc30363f519e2f5328
Subroto Banerjee <subroto.2003@gmail.com>
2025-07-14 10:28:51 +0530

    basic setup for channel

---
+++ lib/core/utils/consts.dart
@@ -0,0 +1,7 @@
+abstract class Constants {
+  // Name of the Realtime channel
+  static const String channelName = 'canvas';
+  
+  // Name of the broadcast event
+  static const String broadcastEventName = 'canvas';
+}

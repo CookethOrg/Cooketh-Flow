9885d5b67cc3fa18e6fdf7d27cc9f9514ab07a25
Subroto Banerjee <subroto.2003@gmail.com>
2025-07-15 00:48:11 +0530

    minor changes

--- lib/features/workspace/pages/desktop/workspace_desktop.dart
+++ lib/features/workspace/pages/desktop/workspace_desktop.dart
@@ -1,5 +1,6 @@
 import 'package:cookethflow/core/helpers/responsive_layout.helper.dart' as rh;
 import 'package:cookethflow/core/theme/colors.dart';
+import 'package:cookethflow/features/workspace/widgets/toolbar.dart';
 import 'package:cookethflow/features/workspace/widgets/workspace_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -10,123 +11,174 @@ class WorkspaceDesktop extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    bool device = rh.ResponsiveLayoutHelper.getDeviceType(context) == rh.DeviceType.desktop;
+    bool device =
+        rh.ResponsiveLayoutHelper.getDeviceType(context) ==
+        rh.DeviceType.desktop;
 
     return Scaffold(
       backgroundColor: const Color(0xFFFF8F8F8),
       // body: PhosphorIconsWidget(),
       // body: ColorPickerWidget(
       // onColorChanged: (Color primaryColor) {
-        // handle color change here
+      // handle color change here
       // },}
-          body: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 40.w, vertical: 40.h),
-            child: Stack(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 40.w, vertical: 40.h),
+        child: Stack(
+          children: [
+            // Top Bar
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                // Top Bar
+                // Back & Title
                 Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      // Back & Title
-                      Row(
-                        children: [
-                          //workspace Drawer
-                          const WorkspaceDrawer(),
-                          SizedBox(width: 20.w),
-                          //Undo/redo
-                          Container(
-                            padding: EdgeInsets.symmetric(
-                              horizontal:
-                                  device == rh.DeviceType.desktop ? 24.w : 32.w,
-                              vertical: 16.h,
-                            ),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(12.r),
-                              border: Border.all(
-                                color: const Color(0xFFD9D9D9),
-                                width: 1.2,
-                              ),
-                            ),
-                            child: SizedBox(
-                              height: 56.h,
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  IconButton(
-                                    onPressed: () {},
-                                    icon: Icon(
-                                      PhosphorIconsRegular.arrowArcLeft,
-                                      size: 32.sp,
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  _verticalDivider(), // Use custom divider
-                                  IconButton(
-                                    onPressed: () {},
-                                    icon: Icon(
-                                      PhosphorIconsRegular.arrowArcRight,
-                                      size: 32.sp,
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
+                  children: [
+                    //workspace Drawer
+                    const WorkspaceDrawer(),
+                    SizedBox(width: 20.w),
+                    //Undo/redo
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal:
+                            device == rh.DeviceType.desktop ? 24.w : 32.w,
+                        vertical: 16.h,
                       ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12.r),
+                        border: Border.all(
+                          color: const Color(0xFFD9D9D9),
+                          width: 1.2,
+                        ),
+                      ),
+                      child: SizedBox(
+                        height: 56.h,
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                PhosphorIconsRegular.arrowArcLeft,
+                                size: 32.sp,
+                                color: Colors.black,
+                              ),
+                            ),
+                            _verticalDivider(), // Use custom divider
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                PhosphorIconsRegular.arrowArcRight,
+                                size: 32.sp,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
 
-                      Row(
+                Row(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        backgroundColor: primaryColor,
+                        padding: EdgeInsets.symmetric(
+                          vertical: 32.h,
+                          horizontal: 24.w,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.r),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
                         children: [
-                          ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              elevation: 0,
-                              backgroundColor: primaryColor,
-                              padding: EdgeInsets.symmetric(
-                                vertical: 32.h,
-                                horizontal: 24.w,
-                              ),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12.r),
-                              ),
+                          Text(
+                            'Export Flowchart',
+                            style: TextStyle(
+                              fontFamily: 'Fredrik',
+                              fontSize: 18.sp,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
                             ),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(
-                                  'Export Flowchart',
-                                  style: TextStyle(
-                                    fontFamily: 'Fredrik',
-                                    fontSize: 18.sp,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                ),
-                                SizedBox(width: 16.w),
-                                Icon(
-                                  PhosphorIconsRegular.export,
-                                  color: Colors.white,
-                                  size: 24.sp,
-                                ),
-                              ],
-                            ),
+                          ),
+                          SizedBox(width: 16.w),
+                          Icon(
+                            PhosphorIconsRegular.export,
+                            color: Colors.white,
+                            size: 24.sp,
                           ),
                         ],
                       ),
-                    ],
+                    ),
+                  ],
+                ),
+                Align(alignment: Alignment.centerRight, child: const ToolBar()),
+                // zoom control
+                Positioned(
+                  bottom: 0.h,
+                  right: 0.w,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20.w,
+                      vertical: 16.h,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color(0XFFD9D9D9), width: 1.2),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12.r),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "100%",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 24.sp,
+                          ),
+                        ),
+                        SizedBox(width: 8.w),
+                        _verticalDivider(),
+                        SizedBox(width: 8.w),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(PhosphorIconsRegular.plus, size: 24.sp),
+                          visualDensity: VisualDensity.compact,
+                        ),
+                        SizedBox(width: 8.w),
+                        _verticalDivider(),
+                        SizedBox(width: 8.w),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(PhosphorIconsRegular.minus, size: 24.sp),
+                          visualDensity: VisualDensity.compact,
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          );
+          ],
+        ),
+      ),
+    );
   }
 }
 
-  Widget _verticalDivider() {
-    return Container(
-      height: 24.h,
-      width: 1.2.w,
-      color: const Color(0xFFD9D9D9),
-      margin: EdgeInsets.symmetric(horizontal: 8.w),);}
+Widget _verticalDivider() {
+  return Container(
+    height: 24.h,
+    width: 1.2.w,
+    color: const Color(0xFFD9D9D9),
+    margin: EdgeInsets.symmetric(horizontal: 8.w),
+  );
+}

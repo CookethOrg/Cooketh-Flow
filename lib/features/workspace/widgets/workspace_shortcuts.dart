688c8c0f771960435defe34943c387a823b11370
swarno-tech <swarnabha.git01@gmail.com>
2025-09-07 18:42:46 +0530

    keyboard shortcuts added

--- lib/features/workspace/widgets/workspace_shortcuts.dart
+++ lib/features/workspace/widgets/workspace_shortcuts.dart
@@ -2,7 +2,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-
 // define intents
 class PointerIntent extends Intent {}
 
@@ -10,12 +9,22 @@ class PanIntent extends Intent {}
 
 class TextIntent extends Intent {}
 
+class StickyNoteIntent extends Intent {}
+
+class ResetIntent extends Intent {}
+
+class ZoomInIntent extends Intent {}
+
+class ZoomOutIntent extends Intent {}
+
 // define shortcuts
 
 final Map<LogicalKeySet, Intent> workspaceShortCut = {
   LogicalKeySet(LogicalKeyboardKey.keyP): PointerIntent(),
   LogicalKeySet(LogicalKeyboardKey.keyA): PanIntent(),
   LogicalKeySet(LogicalKeyboardKey.keyT): TextIntent(),
+  LogicalKeySet(LogicalKeyboardKey.keyS): StickyNoteIntent(),
+  LogicalKeySet(LogicalKeyboardKey.keyR): ResetIntent(),
+  LogicalKeySet(LogicalKeyboardKey.keyZ): ZoomInIntent(),
+  LogicalKeySet(LogicalKeyboardKey.keyX): ZoomOutIntent(),
 };
-
-
